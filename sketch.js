0fa7c5f5c55f8476d2e9989c0e039c1cb3d9f964
Shubh Jain <65031443+Shubh-Jxin@users.noreply.github.com>
2020-08-16 18:10:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -6,77 +6,91 @@ var canvas;
 var world,engine;
 var playerCount;
 var gameState=0;
-var player, game,form, inst;
-var sdg1,sdg2,sdg3,sdg4,sdg5,sdg6,sdg7,sdg8,sdg9,sdg10,sdg11,sdg12,sdg13,sdg14,sdg15,sdg16,sdg17;
-var un,law,logo,start,reward;
+var player, game,form;
 var database;
-var reset;
 var board;
-var allPlayers;
-var pawn1, pawn2,pawn3, pawn4;
-var pawn1_img, pawn2_img,pawn3_img, pawn4_img;
 
-function preload(){
-    pawn1_img= loadImage("pic/green.png");
-}
+
 function setup(){
-    // if(gameState===2){
-        canvas= createCanvas(displayWidth-20,displayHeight-30);
-    // }
+
+    canvas= createCanvas(displayWidth+500,displayHeight);
+    
     engine= Engine.create();   
     world= engine.world;
 
     database= firebase.database();
+    this.sdg1= new Baseclass(150,100,150,150,"pic/sdg1.png");
+        this.sdg2= new Baseclass(300,100,150,150,"pic/sdg2.png");
+        this.sdg3= new Baseclass(450,100,150,150,"pic/sdg3.png");
+        this.sdg4= new Baseclass(600,100,150,150,"pic/sdg4.png");
+        this.sdg5= new Baseclass(900,100,150,150,"pic/sdg5.png");
+        this.sdg6= new Baseclass(1050,100,150,150,"pic/sdg6.png");
+        this.sdg7= new Baseclass(1200,100,150,150,"pic/sdg7.png");
+        this.un= new Baseclass(1370,100,200,200,"pic/un.png");
+        this.sdg8= new Baseclass(1350,275,150,150,"pic/sdg8.png");
+        this.sdg9= new Baseclass(1350,425,150,150,"pic/sdg9.png");
+        this.law= new Baseclass(1370,750,200,200,"pic/law.png");
+        this.sdg10= new Baseclass(1200,750,150,150,"pic/sdg10.png");
+        this.sdg11= new Baseclass(1050,750,150,150,"pic/sdg11.png");
+        this.sdg12= new Baseclass(900,750,150,150,"pic/sdg12.png");
+        this.sdg13= new Baseclass(750,750,150,150,"pic/sdg13.png");
+        this.sdg14= new Baseclass(450,750,150,150,"pic/sdg14.png");
+        this.sdg15= new Baseclass(300,750,150,150,"pic/sdg15.png");
+        this.reward= new Baseclass(125,725,200,200,"pic/reward.png");
+        this.sdg16= new Baseclass(150,550,150,150,"pic/sdg16.png");
+        this.sdg17= new Baseclass(150,400,150,150,"pic/sdg17.png");
+        this.start= new Baseclass(150,250,150,150,"pic/start.png");
+        this.logo= new Baseclass(displayWidth/2,displayHeight/2,1000,400,"pic/sdg.png");  
+        
 
-    
+   
 
     game=new Game();
     game.getState();
-    
 
-    reset= createButton("reset");
-    reset.position(1300,200);
 
-    // if(gameState===2){]
-    
-    
-    //board.display();
-    // }
-    //if(gameState===2){
-//         board=new Board();
-//         board.start();
-        
-      //  }
-        //  /game.play();
-        
     }
     
     function draw(){  
-        if(gameState===2){
-            if(playerCount <= 4){
-                background(0);
-                board.display();
-                game.play();
-             //game.play();
-            } 
-    }
+     background(0);
+    
+
+          
+    fill(255)
+        rectMode(CENTER);
+        rect(750,100,150,150);
+        rect(1350,575,150,150);
+        rect(600,750,150,150)
+        // fill()
+        // rect(displayWidth/2-19,displayHeight/2,1000,400 )
+        noFill();
+        stroke(255);
+        strokeWeight(3);
+        rect(1370,750,190,190);
+        this.sdg1.display();
+        this.sdg2.display();
+        this.sdg3.display();
+        this.sdg4.display();
+        this.sdg5.display();
+        this.sdg6.display();
+        this.sdg7.display();
+        this.un.display();
+        this.sdg8.display();
+        this.sdg9.display();
+        this.law.display(); 
+        this.sdg10.display();
+        this.sdg11.display();
+        this.sdg12.display();
+        this.sdg13.display();
+        this.sdg14.display();
+        this.sdg15.display();
+        this.reward.display();
+        this.sdg16.display();
+        this.sdg17.display();
+        this.start.display();  
+        this.logo.display(); 
 
     Engine.update(engine);
 
-    // if(gameState===2){
-        
-    // }
-    // if(gameState===2){
-    //     board=new Board();
-    //     board.start();
-    //     board.display();
-    // }
-    
-     reset.mousePressed(()=>{
-        database.ref("/").update({
-            playerCount:0,
-            gameState:0
-        })
-    })
-    
+
 }
